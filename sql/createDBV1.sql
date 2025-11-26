82a75adf577b38d46d209d6d4ea91c9c37c15952
Gavril Marian <B00165945@mytudublin.ie>
2025-11-26 21:07:49 +0000

    Added new member HTML,it displays on the web server

---
+++ sql/createDBV1.sql
@@ -0,0 +1,9 @@
+create database aquaticCentre;
+use aquaticCentre;
+create table aquaticCentre (
+    id INT AUTO_INCREMENT,
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    Phone Number VARCHAR(30),
+    PRIMARY KEY(id)
+);
