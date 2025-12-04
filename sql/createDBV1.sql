42947aaf74b8c60544346cb2da1eec750097b33b
Gavril Marian <B00165945@mytudublin.ie>
2025-12-04 14:21:14 +0000

    Add Version 2 database table with membership_type and price fields

--- sql/createDBV1.sql
+++ sql/createDBV1.sql
@@ -1,6 +1,6 @@
 create database aquaticCentre;
 use aquaticCentre;
-create table aquaticCentre (
+create table aquaticCentre_v1 (
     id INT AUTO_INCREMENT,
     firstname VARCHAR(50),
     lastname VARCHAR(50),
