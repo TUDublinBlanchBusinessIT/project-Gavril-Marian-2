fb3b6d0d806004dc9a8f7e33db3c73d55a449c8b
Gavril Marian <B00165945@mytudublin.ie>
2025-12-10 00:21:52 +0000

    Implemented the login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+if (isset($_SESSION['username'])) {
+    header("Location: displayMembers_V5.php");
+    exit();
+}
+?>
+
+<h2>Login</h2>
+
+<form method="POST" action="loginCheck.php">
+    Username:<br>
+    <input type="text" name="username" required><br><br>
+
+    Password:<br>
+    <input type="password" name="password" required><br><br>
+
+    <input type="submit" value="Login">
+</form>
