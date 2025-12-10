f3cdb4b159b061058824b1eeaeaffc9663fa9a80
Gavril Marian <B00165945@mytudublin.ie>
2025-12-10 03:14:18 +0000

    Wrote about my project in README.MD

--- member.php
+++ member.php
@@ -4,14 +4,13 @@ class member {
 
     public $firstname;
     public $lastname;
-
     
     public function __construct($firstname, $lastname) {
         $this->firstname = $firstname;
         $this->lastname = $lastname;
     }
 
-    
+
     public function getFullName() {
         return $this->firstname . " " . $this->lastname;
     }
